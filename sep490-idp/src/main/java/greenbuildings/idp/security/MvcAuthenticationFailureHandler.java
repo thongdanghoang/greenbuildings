6de3cdb8ffb8c3af4272d14f5bd5f7c21e1a76d3
Tran Gia Bao <bao.trangia@elca.vn>
2025-03-01 23:23:44 +0700

    fix: Redirect URL missing context-path

--- sep490-idp/src/main/java/greenbuildings/idp/security/MvcAuthenticationFailureHandler.java
+++ sep490-idp/src/main/java/greenbuildings/idp/security/MvcAuthenticationFailureHandler.java
@@ -3,6 +3,7 @@ package greenbuildings.idp.security;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,18 +11,23 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 
 import java.io.IOException;
 
+@Slf4j
 public class MvcAuthenticationFailureHandler implements AuthenticationFailureHandler {
-
+    
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-        AuthenticationException exception) throws IOException, ServletException {
-
+                                        AuthenticationException exception) throws IOException, ServletException {
+        
         if (exception.getClass().isAssignableFrom(UsernameNotFoundException.class)
             || exception.getClass().isAssignableFrom(BadCredentialsException.class)) {
             String errorKey = "login.error.badCredentials";
-
-            response.sendRedirect("/login?error=" + errorKey);
+            String contextPath = request.getContextPath();
+            if (contextPath.isEmpty()) {
+                log.warn("Missing context path");
+            }
+            
+            response.sendRedirect(contextPath + "/login?error=" + errorKey);
         }
-
+        
     }
 }
