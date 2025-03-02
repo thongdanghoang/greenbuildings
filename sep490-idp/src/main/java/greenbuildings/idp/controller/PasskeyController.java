b513b77fe77cd73bd23044231a61a0f1b03e495f
Tran Gia Bao <bao.trangia@elca.vn>
2025-03-02 23:52:35 +0700

    feature: Redirect when login with passkey in OAUTH2 Flow

--- sep490-idp/src/main/java/greenbuildings/idp/controller/PasskeyController.java
+++ sep490-idp/src/main/java/greenbuildings/idp/controller/PasskeyController.java
@@ -1,10 +1,13 @@
 package greenbuildings.idp.controller;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
@@ -15,6 +18,8 @@ import greenbuildings.idp.security.MvcUserContextData;
 import greenbuildings.idp.service.AuthenticatorService;
 import greenbuildings.idp.service.impl.LoginService;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @Controller
@@ -24,18 +29,26 @@ public class PasskeyController {
 
     private final AuthenticatorService authenticatorService;
     private final LoginService loginService;
-
-    @PostMapping("/passkey/login")
-    public String login(@RequestBody CredentialsVerification verification, SessionStatus sessionStatus) {
+    
+    @PostMapping(value = "/passkey/login")
+    @ResponseBody
+    public Map<String, String> login(@RequestBody CredentialsVerification verification, SessionStatus sessionStatus, HttpServletRequest request) {
+        Map<String, String> map = new HashMap<>();
+        String url = request.getHeader(HttpHeaders.ORIGIN) + request.getContextPath() + "/account";
         try {
             var user = authenticatorService.authenticate(verification);
             loginService.login(user);
+            sessionStatus.setComplete(); //Remove challenge in session
         } catch (EntityNotFoundException | NoSuchElementException ex) {
-            return "redirect:/login?message=login.error.noPasskey";
+            url = "/login?message=login.error.noPasskey";
         }
-
-        sessionStatus.setComplete(); //Remove challenge in session
-        return "redirect:/account";
+        try {
+            url = ((DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST")).getParameterMap().get("redirect_uri")[0];
+        } catch (NullPointerException | IndexOutOfBoundsException ex) {
+            // swallow, since this request not called from FE
+        }
+        map.put("url", url);
+        return map;
     }
 
     @PostMapping("/passkey/register")
