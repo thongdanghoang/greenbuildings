1d9c9b79f34ef19c8ac559599ae9093ad81afb69
Tran Gia Bao <bao.trangia@elca.vn>
2025-04-06 22:37:22 +0700

    feature: Drop tax code
    Signed-off-by: Tran Gia Bao <bao.trangia@elca.vn>

--- sep490-commons/api/src/main/java/greenbuildings/commons/api/events/PendingEnterpriseRegisterEvent.java
+++ sep490-commons/api/src/main/java/greenbuildings/commons/api/events/PendingEnterpriseRegisterEvent.java
@@ -9,8 +9,7 @@ import lombok.Builder;
 public record PendingEnterpriseRegisterEvent(
         @NotBlank String name,
         @NotBlank @Pattern(regexp = CommonConstant.EMAIL_PATTERN) String email,
-        @NotBlank @Pattern(regexp = CommonConstant.VIETNAM_ENTERPRISE_HOTLINE_PATTERN) String hotline,
-        @NotBlank @Pattern(regexp = CommonConstant.VIETNAME_TAX_CODE) String taxCode
+        @NotBlank @Pattern(regexp = CommonConstant.VIETNAM_ENTERPRISE_HOTLINE_PATTERN) String hotline
 ) {
     public static final String TOPIC = "enterprise-create-event";
 }
