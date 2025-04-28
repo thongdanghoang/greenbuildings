928e0c7ad07fe936f5a288de6d196f68dac275ab
Thống <thongdhse160015@fpt.edu.vn>
2025-04-28 15:03:23 +0700

    chores

--- sep490-enterprise/src/main/java/greenbuildings/enterprise/EnterpriseApplication.java
+++ sep490-enterprise/src/main/java/greenbuildings/enterprise/EnterpriseApplication.java
@@ -1,7 +1,10 @@
 package greenbuildings.enterprise;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -13,5 +16,16 @@ public class EnterpriseApplication {
     public static void main(String[] args) {
         SpringApplication.run(EnterpriseApplication.class, args);
     }
+    
+//    @Bean
+//    public FlywayMigrationStrategy cleanMigrateStrategy() {
+//        return new FlywayMigrationStrategy() {
+//            @Override
+//            public void migrate(Flyway flyway) {
+//                flyway.clean();
+//                flyway.migrate();
+//            }
+//        };
+//    }
     
 }
--- sep490-idp/src/main/java/greenbuildings/idp/IdentityProvider.java
+++ sep490-idp/src/main/java/greenbuildings/idp/IdentityProvider.java
@@ -1,7 +1,10 @@
 package greenbuildings.idp;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -15,5 +18,16 @@ public class IdentityProvider {
     public static void main(String[] args) {
         SpringApplication.run(IdentityProvider.class, args);
     }
+    
+//    @Bean
+//    public FlywayMigrationStrategy cleanMigrateStrategy() {
+//        return new FlywayMigrationStrategy() {
+//            @Override
+//            public void migrate(Flyway flyway) {
+//                flyway.clean();
+//                flyway.migrate();
+//            }
+//        };
+//    }
     
 }
