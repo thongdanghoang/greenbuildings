e54fc3651bb87c5c3ddc7fc2e6fd5a1d685f67c0
GiaBaor <92243205+GiaBaorr@users.noreply.github.com>
2025-04-30 00:09:12 +0700

    feature: Sort invitation by date and status by default. Add validatio… (#345)
    feature: Sort invitation by date and status by default. Add validation when link group -> tenant, show confirm when reject invitations
    
    Signed-off-by: Tran Gia Bao

--- sep490-frontend/src/app/modules/enterprise/components/enterprise-invitation/enterprise-invitation.component.ts
+++ sep490-frontend/src/app/modules/enterprise/components/enterprise-invitation/enterprise-invitation.component.ts
@@ -3,21 +3,28 @@ import {InvitationResponse} from '@models/tenant';
 import {TranslateService} from '@ngx-translate/core';
 import {InvitationDTO, InvitationStatus} from '@models/enterprise';
 import {InvitationService} from '@services/invitation.service';
+import {SubscriptionAwareComponent} from '@shared/directives/subscription-aware.component';
+import {ModalProvider} from '@shared/services/modal-provider';
 import {ToastProvider} from '@shared/services/toast-provider';
+import {takeUntil} from 'rxjs';
 
 @Component({
   selector: 'app-enterprise-invitation',
   templateUrl: './enterprise-invitation.component.html',
   styleUrl: './enterprise-invitation.component.css'
 })
-export class EnterpriseInvitationComponent implements OnInit {
+export class EnterpriseInvitationComponent extends SubscriptionAwareComponent implements OnInit {
   invitations: InvitationDTO[] = [];
 
   constructor(
+    private readonly msgService: ToastProvider,
+    private readonly modalProvider: ModalProvider,
     private readonly invitationService: InvitationService,
     public readonly translate: TranslateService,
     private readonly messageService: ToastProvider
-  ) {}
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
     this.fetchPendingInvitation();
@@ -49,16 +56,41 @@ export class EnterpriseInvitationComponent implements OnInit {
     };
     this.invitationService.updateStatus(response).subscribe(() => {
       this.fetchPendingInvitation();
+      this.msgService.success({
+        summary: this.translate.instant('common.success'),
+        detail: this.translate.instant('common.saveSuccess')
+      });
     });
   }
 
   rejectInvitation(invitation: InvitationDTO): void {
-    const response: InvitationResponse = {
-      id: invitation.id,
-      status: InvitationStatus.REJECTED
-    };
-    this.invitationService.updateStatus(response).subscribe(() => {
-      this.fetchPendingInvitation();
-    });
+    this.modalProvider
+      .showConfirm({
+        message: this.translate.instant('sentInvitation.rejectConfirm'),
+        header: this.translate.instant('common.confirmHeader'),
+        icon: 'pi pi-info-circle',
+        acceptButtonStyleClass: 'p-button-danger p-button-text min-w-20',
+        rejectButtonStyleClass: 'p-button-contrast p-button-text min-w-20',
+        acceptIcon: 'none',
+        acceptLabel: this.translate.instant('common.accept'),
+        rejectIcon: 'none',
+        rejectLabel: this.translate.instant('common.reject')
+      })
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((result: boolean): void => {
+        if (result) {
+          const response: InvitationResponse = {
+            id: invitation.id,
+            status: InvitationStatus.REJECTED
+          };
+          this.invitationService.updateStatus(response).subscribe(() => {
+            this.fetchPendingInvitation();
+            this.msgService.success({
+              summary: this.translate.instant('common.success'),
+              detail: this.translate.instant('sentInvitation.reject')
+            });
+          });
+        }
+      });
   }
 }
