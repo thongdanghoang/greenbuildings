329b44a5ffb4edbe4b82a222623ab685bf309bb0
Thống <thongdhse160015@fpt.edu.vn>
2025-05-03 06:36:14 +0700

    bugfix: UX/UI

--- sep490-frontend/src/app/modules/enterprise/components/payment/payment.component.ts
+++ sep490-frontend/src/app/modules/enterprise/components/payment/payment.component.ts
@@ -107,15 +107,15 @@ export class PaymentComponent extends SubscriptionAwareComponent implements OnIn
   getStatusClass(status: string): string {
     switch (status) {
       case PaymentStatus[PaymentStatus.PENDING]:
-        return 'bg-yellow-100 text-yellow-800 font-medium px-2 py-1 rounded-md';
+        return 'bg-yellow-100 text-yellow-800';
       case PaymentStatus[PaymentStatus.PAID]:
-        return 'bg-green-100 text-green-800 font-medium px-2 py-1 rounded-md';
+        return 'bg-green-100 text-green-800';
       case PaymentStatus[PaymentStatus.PROCESSING]:
-        return 'bg-blue-100 text-blue-800 font-medium px-2 py-1 rounded-md';
+        return 'bg-blue-100 text-blue-800';
       case PaymentStatus[PaymentStatus.CANCELLED]:
-        return 'bg-red-100 text-red-800 font-medium px-2 py-1 rounded-md';
+        return 'bg-red-100 text-red-800';
       default:
-        return 'bg-gray-100 text-gray-800 font-medium px-2 py-1 rounded-md';
+        return 'bg-gray-100 text-gray-800';
     }
   }
 
