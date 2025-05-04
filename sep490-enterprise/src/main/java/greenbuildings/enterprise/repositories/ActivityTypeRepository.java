207b50c7c1ae1bff8e319dca6aeb97e2ee47378a
Tran Gia Bao <bao.trangia@elca.vn>
2025-05-04 21:20:17 +0700

    fix: unique type name in the same building
    Signed-off-by: Tran Gia Bao <bao.trangia@elca.vn>

--- sep490-enterprise/src/main/java/greenbuildings/enterprise/repositories/ActivityTypeRepository.java
+++ sep490-enterprise/src/main/java/greenbuildings/enterprise/repositories/ActivityTypeRepository.java
@@ -2,6 +2,7 @@ package greenbuildings.enterprise.repositories;
 
 import greenbuildings.enterprise.entities.ActivityTypeEntity;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -24,6 +25,6 @@ public interface ActivityTypeRepository extends JpaRepository<ActivityTypeEntity
     )
     Page<UUID> findByName(UUID buildingId, String name, Pageable pageable);
     
-    @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END FROM ActivityTypeEntity b WHERE b.name = :name")
-    boolean existsByNameActivityTypeInTenant(@NotBlank String name);
+    @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END FROM ActivityTypeEntity b WHERE b.name = :name and b.building.id = :buildingId")
+    boolean existsByNameActivityTypeInTenant(@NotNull UUID buildingId, @NotBlank String name);
 } 
